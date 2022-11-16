9057f70aabd8df2978717e56ac74c27128fcf1da
guanch92 <guancheng92@gmail.com>
2022-11-17 05:09:39 +0800

    fix client files path

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 const controller = require("./routes.js");
 
 //app middleware
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
